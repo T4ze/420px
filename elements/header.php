6820d181373acf43a1589575f88d71204ae3bed0
Florent Youinou <youino_f@epita.fr>
2017-05-28 12:52:00 +0200

    css: fetch from bulma cdn

--- elements/header.php
+++ elements/header.php
@@ -1,8 +1,9 @@
 <head>
   <meta charset="utf-8">
   <title>420px: <?php echo $title; ?></title>
-  <link rel="stylesheet" type="text/css" href="css/bulma.css"/>
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css"/>
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+  <link rel="stylesheet" type="text/css" href="css/style.css"/>
   <script src="js/jquery.min.js"></script>
   <script src="js/bulma.js"></script>
 </head>
